356e8db6f7572b040245289905215d374a0e101a
chinnaragamsetti <ragamsettirajkumar@gmail.com>
2024-01-29 05:15:10 +0000

    main

--- src/components/RegistrationForm/index.js
+++ src/components/RegistrationForm/index.js
@@ -3,55 +3,84 @@ import {Component} from 'react'
 import './index.css'
 
 class RegistrationForm extends Component {
-  state = {submit: false,firstName:'',lastName:'',firstnameerrormsg:false,lastnameerrormsg:fals}
-  
-  onSubmitForm =event=> {
-      event.preventDefault()
-      const {firstName,lastName}=this.state
-
-      if(firstName==='' && lastName===''){
-          this.setState(prevState=>({firstnameerrormsg:!prevState.firstnameerrormsg,lastnameerrormsg:!prevState.lastnameerrormsg}))
-      }
-      else{
-        this.setState(prevState => ({submit: !prevState.submit}))
-      }
-    
+  state = {
+    submit: false,
+    firstName: '',
+    lastName: '',
+    firstnameerrormsg: false,
+    lastnameerrormsg: false,
   }
 
-  onChangefirstname=(event)=>{
-      if(event.target.value ===''){
-            this.setState(prevState=>({firstnameerrormsg:!prevState.firstnameerrormsg}))
-      }
-      else{
-        this.setState(prevState=>({firstName:event.target.value}))
-      }
+  onSubmitForm = event => {
+    event.preventDefault()
+    const {firstName, lastName} = this.state
+
+    if (firstName === '' || lastName === '') {
+      this.setState(prevState => ({
+        firstnameerrormsg: !prevState.firstnameerrormsg,
+        lastnameerrormsg: !prevState.lastnameerrormsg,
+      }))
+    } else {
+      this.setState(prevState => ({submit: !prevState.submit}))
+    }
   }
 
-  onChangelastname=(event)=>{
-      if(event.target.value===''){
-            this.setState(prevState=>({lastnameerrormsg:!prevState.lastnameerrormsg}))
-      }
-      else{
-        this.setState(prevState=>({lastName:event.target.value}))
-      }
+  onChangefirstname = event => {
+    if (event.target.value === '') {
+      this.setState(prevState => ({
+        firstnameerrormsg: !prevState.firstnameerrormsg,
+      }))
+    } else {
+      this.setState(prevState => ({firstName: event.target.value}))
+    }
+  }
+
+  onChangelastname = event => {
+    if (event.target.value === '') {
+      this.setState(prevState => ({
+        lastnameerrormsg: !prevState.lastnameerrormsg,
+      }))
+    } else {
+      this.setState(prevState => ({lastName: event.target.value}))
+    }
   }
 
   submitForm = () => {
-      const {firstName,lastName,errormsg}=this.state
+    const {firstName, lastName, errormsg} = this.state
     return (
-        <form className="formcontainer" onSubmit={this.onSubmitForm}>
-            <label className="firstname" htmlFor="first">
-                FIRST NAME
-            </label>
-            <input className="firstnameinput" type='text' value={firstName} id='first' onChange={this.onChangefirstname}/>
-            {errormsg && <p className='error'>Required</p>}
-            <label className="firstname" htmlFor="last">
-                LAST NAME
-            </label>
-            <input className="firstnameinput" type='text' value={lastName} id='last' onChange={this.onChangelastname}/>
-            {errormsg && <p className='error'>Required</p>}
-            <button className='submitbutton'>Submit</button>
-        </form>
+      <form className="formcontainer" onSubmit={this.onSubmitForm}>
+        <label className="firstname" htmlFor="first">
+          FIRST NAME
+        </label>
+        <input
+          className="firstnameinput"
+          placeholder="First name"
+          type="text"
+          value={firstName}
+          id="first"
+          onBlur={this.onChangefirstname}
+        />
+        {errormsg && <p className="error">Required</p>}
+        <label className="firstname" htmlFor="last">
+          LAST NAME
+        </label>
+        <input
+          placeholder="Last name"
+          className="firstnameinput"
+          type="text"
+          value={lastName}
+          id="last"
+          onBlur={this.onChangelastname}
+        />
+        {errormsg && <p className="error">Required</p>}
+        <button
+          type="button"
+          className="submitbutton"
+          onClick={this.onSubmitFrom}
+        >
+          Submit
+        </button>
+      </form>
     )
   }
 
@@ -60,25 +89,35 @@ class RegistrationForm extends Component {
   }
 
   successfullContainer = () => (
-    <div className="anotherSubmitcontainer">
+    <div className="anothersubmitcontainer">
       <img
         src="https://assets.ccbp.in/frontend/react-js/success-icon-img.png"
         alt="success"
         className="image"
       />
       <p className="succesfullpara">Submitted Successfully</p>
-      <button className="anotherresponsebutton" onClick={this.onSubmitanother}>
+      <button
+        className="anotherresponsebutton"
+        type="button"
+        onClick={this.onSubmitanother}
+      >
         Submit Another Response
       </button>
     </div>
   )
-  
+
   render() {
     const {submit} = this.state
     return (
       <div className="maincontainer">
-        <h1 className="heading">Registration</h1>
-        {submit ? <div>{this.successfullContainer()}<div/> : <div>{this.submitForm()}<div/>}
+        <div className="subcontainer">
+          <h1 className="heading">Registration</h1>
+          {submit ? (
+            <div>{this.successfullContainer()}</div>
+          ) : (
+            <div>{this.submitForm()}</div>
+          )}
+        </div>
       </div>
     )
   }
